fc8ad6fd3f68e26fedfbd764b81104c468ba1dbc
kgayathrii83 <51078831+kgayathrii83@users.noreply.github.com>
2024-07-22 16:48:01 +0530

    Update PaymentInitializer.java

--- micro-investment/payment/src/main/java/com/api/payment/PaymentInitializer.java
+++ micro-investment/payment/src/main/java/com/api/payment/PaymentInitializer.java
@@ -8,6 +8,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication(scanBasePackages = {"com.api"})
 public class PaymentInitializer extends SpringBootServletInitializer {
     public static void main(String[] args) {
+    	System.setProperty("http.proxyHost", "userproxy.rbsgrp.net");
+        System.setProperty("http.proxyPort", "8080");
         SpringApplication.run(com.api.payment.PaymentInitializer.class, args);
     }
 }
