b7205b1b9ca5734dcc4ff490d9832f7274feff83
kgayathrii83 <51078831+kgayathrii83@users.noreply.github.com>
2024-07-22 15:11:45 +0530

    Update PaymentInitializer.java

--- micro-investment/payment/src/main/java/com/api/payment/PaymentInitializer.java
+++ micro-investment/payment/src/main/java/com/api/payment/PaymentInitializer.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages = {"com.api.payment"})
+@SpringBootApplication(scanBasePackages = {"com.api"})
 public class PaymentInitializer extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(com.api.payment.PaymentInitializer.class, args);
